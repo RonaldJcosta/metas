5c21f23e00ca09e49299e86606f57c898932697b
Ronald <ronald.jc@hotmail.com>
2021-01-08 17:11:41 -0300

    Foi criado o model e as rotas

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Instalar modulo que reiniciarar o servidor toda vez que ouver alteração no có
 
 Instalar o banco de dados MongoDB
 ### npm install --save mongodb
+
+Instalar o mongoose - traduz os dados do banco de dados para o objeto JavaScript para ser
+usado na aplicação
+### npm install --save mongoose
+
+Permitir acesso a API
+### npm install --save cors
--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('./models/Metas')
+
+const Meta = mongoose.model('Meta')
 
 const app = express()
 
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost/metas', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
 app.get('/metas', async (req, res) => {
+
+    await Meta.find().then((metas) => {
+        return res.json({
+            error: false,
+            metas
+        })
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true,
+            message: 'Nenhum registro encontrado!'
+        })
+    })
+
    return res.json({ nome: "Ronald"})
+})
+
+app.post('/metas', async (req, res) => {
+
+    await Meta.create(req.body, (error) => {
+        if(error) return res.status(400).json({
+            error: true,
+            message: 'Erro: Não foi possível cadastrar a meta'
+        })
+    })
+
+    return res.json({
+        error: false,
+        message: 'Meta cadastrada com sucesso!'
+    })
+
 })
 
 app.listen(8080, () => {
